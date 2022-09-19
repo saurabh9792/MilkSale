89f9244f0f5f56b7a21bcf6ac3d13d15aee4873b
Saurabh Shukla <saurabhshukla8813@gmail.com>
2022-09-19 23:42:29 +0530

    Commit the First Change

---
+++ MilkShale/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MilkShale.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+
+        public ActionResult Farm()
+        {
+            return View();
+        }
+        public ActionResult Product()
+        {
+            return View();
+        }
+        [ChildActionOnly]
+        public ActionResult HeaderPageContent()
+        {
+            
+            return PartialView("~/Views/Shared/_Header.cshtml");
+        }
+
+        [ChildActionOnly]
+        public ActionResult FooterPageContent()
+        {
+            
+            return PartialView("~/Views/Shared/_Footer.cshtml");
+        }
+
+    }
+}
